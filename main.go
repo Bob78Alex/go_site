ec31fc367f4480f6e3c4207b9b8346895512daf2
Alex Babenko <bob78alex@gmail.com>
2020-06-04 15:06:59 -0400

    Adding server functionality

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 )
 
@@ -12,15 +14,51 @@ func init() {
 }
 func main() {
 
-	http.HandlerFunc("/index", index)
-	http.HandlerFunc("/index", about)
-	http.HandlerFunc("/contact", contact)
-	http.HandlerFunc("/apply", apply)
+	http.HandleFunc("/", index)
+	http.HandleFunc("/about", about)
+	http.HandleFunc("/contact", contact)
+	http.HandleFunc("/apply", apply)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
 
 	http.ListenAndServe(":8080", nil)
 }
 
-func index(w http.ResponseWriter, req *http.Request)   {}
-func about(w http.ResponseWriter, req *http.Request)   {}
-func contact(w http.ResponseWriter, req *http.Request) {}
-func apply(w http.ResponseWriter, req *http.Request)   {}
+func index(w http.ResponseWriter, req *http.Request) {
+	err := tpl.ExecuteTemplate(w, "index.gohtml", nil)
+	if err != nil {
+		log.Println("Log", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+	fmt.Println("No errors found")
+	fmt.Println(req.URL.Path)
+}
+func about(w http.ResponseWriter, req *http.Request) {
+	err := tpl.ExecuteTemplate(w, "about.gohtml", nil)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+}
+func contact(w http.ResponseWriter, req *http.Request) {
+	err := tpl.ExecuteTemplate(w, "contact.gohtml", nil)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+}
+func apply(w http.ResponseWriter, req *http.Request) {
+	var first string
+	if req.Method == http.MethodPost {
+		first = req.FormValue("fname")
+	}
+
+	err := tpl.ExecuteTemplate(w, "apply.gohtml", first)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+}
